93a03c98ef2232869a2db8fb3a290f66f2eb28e3
Nikita <nikita@platform500.com>
2022-06-25 14:36:14 +0300

    finish back-end

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 
-import { registerValidation } from './validations/auth.js';
+import { registerValidation, LoginValidation, postCreateValidation } from './validations.js';
 
-import checkAuth from './utils/checkAuth.js';
-
-import * as UserController from './controllers/UserController.js';
+import { checkAuth, handlevalidationErrors } from './utils/index.js';
+import { UserController, PostController } from './controllers/index.js';
 
 mongoose
   .connect('mongodb+srv://admin:admin@cluster0.uzwku.mongodb.net/blog?retryWrites=true&w=majority')
@@ -13,11 +13,42 @@ mongoose
   .catch((err) => console.log('DB error', err));
 
 const app = express();
-app.use(express.json());
 
-app.post('/auth/login', UserController.login);
-app.post('/auth/register', registerValidation, UserController.register);
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', LoginValidation, handlevalidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handlevalidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handlevalidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handlevalidationErrors,
+  PostController.update,
+);
 
 app.listen(3333, (err) => {
   if (err) {
